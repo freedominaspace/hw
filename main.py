811c40a41b86f099d6d8f6116574c22425497cf1
test1e <38080700+freedominaspace@users.noreply.github.com>
2018-09-25 15:59:10 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,19 +1,14 @@
-#1-1.2
+1-1.2
 
 name = input("Input your full name?")
-age = str(input("How old are you?"))
+age = input("age?")
 ques = input("Are u a man?")
-ans = ('woman', 'man')[ques == "yes"]
-man = "male"
-woman = "female"
-yes = True
-no = False
+ans = ('woman', 'man')[ques == "yes" ]
 
-print("You are " + name + ", Вам " + age + "лет, " + ans + " " + ques)
+print("You are " + name + ", Вам " + f"{int(age)} " + "лет, " + ans + " " + ques)
 
 
-
-#2.1
+# 2.1
 
 a = input("Введите 1 число ")
 b = input("Введите 2 число ")
@@ -22,11 +17,30 @@ a, b = b, a
 print(a, b)
 
 #2
+#var2 Сокращённый вариант(додумал не сам) 
 
-a = input("Введите 1 число ")
-b = input("Введите 2 число ")
+a = int(input("Введите 1 число "))
+b = int(input("Введите 2 число "))
+
+a += b
+b = (a - b)
+a -= b
+
 print(a, b)
 
-c = [b, a]
-print(c)
+# #var1
+#
+# a = a + b
+# b = a - b
+# a = a - b
 
+#2.2
+
+a = 3
+b = 2
+
+c = a
+a = b
+b = c
+
+print(a, b)
